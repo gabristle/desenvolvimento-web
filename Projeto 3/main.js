27c4e545b2808def4671c07d988221c48f606106
Gabriella Costa <gabx2205@gmail.com>
2024-06-10 15:05:45 -0300

    feat project 3

---
+++ Projeto 3/main.js
@@ -0,0 +1,8 @@
+let firstName = prompt('Digite seu primeiro nome:');
+let lastName = prompt('Digite seu último nome:');
+let age = Number(prompt('Digite sua idade:'));
+const age2 = 2;
+
+age = age + age2;
+
+console.log(`Meu nome é ${firstName} ${lastName} e eu tenho ${age} anos de idade.`);
