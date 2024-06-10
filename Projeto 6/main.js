3fa04c7974763c6d6041cc0973e39790ff876c9d
Gabriella Costa <gabx2205@gmail.com>
2024-06-10 16:34:27 -0300

    feat project 6

---
+++ Projeto 6/main.js
@@ -0,0 +1,8 @@
+let friends = ['Gabriella', 'Rafael', 'Tina', 'Gustavo', 'Marquinhos', 10];
+console.log(friends);
+
+friends[5] = 'Nami'; //Replace
+console.log(friends[5]);
+
+friends[6] = 'Mingau'; //Add items
+console.log(friends);
