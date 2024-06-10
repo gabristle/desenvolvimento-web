51d42f3adec2e27674dea123eb58a76b14e61a33
Gabriella Costa <gabx2205@gmail.com>
2024-06-10 14:49:29 -0300

    feat project 2

---
+++ Projeto 2/main.js
@@ -0,0 +1,7 @@
+let firstName = 'Gabriella';
+let age = 20;
+
+console.log(firstName);
+console.log('Meu nome é ' + firstName + '!');
+console.log(`Meu nome é ${firstName}!`);
+console.log(`Eu tenho ${age} anos de idade.`);
